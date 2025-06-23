93d29d07fe1b1ffc995c1323d5eb1b20cb8ffd44
T14Xploit <khrystyna.tyvoniuk@gmail.com>
2025-06-23 21:14:02 +0200

    fixed better auth, added shadcn components

--- src/app/api/auth/[...all]/route.ts
+++ src/app/api/auth/[...all]/route.ts
@@ -1,3 +1,4 @@
-import { auth } from "@/lib/auth";
-
-export const { GET, POST } = auth.handler;
+import { auth } from "@/lib/auth"; // path to your auth file
+import { toNextJsHandler } from "better-auth/next-js";
+ 
+export const { POST, GET } = toNextJsHandler(auth);
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -9,16 +9,6 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
   },
-  socialProviders: {
-    google: {
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    },
-  },
-  session: {
-    expiresIn: 60 * 60 * 24 * 7, // 7 days
-    updateAge: 60 * 60 * 24, // 1 day
-  },
+ 
 });
 
-export type Session = typeof auth.$Infer.Session;
